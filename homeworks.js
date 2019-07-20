87ffed3ca2e0a18014d3a5cf65e9de2590dda2c2
Davit Vardanyan <Davit.Vardanyan@istc.com>
2019-07-20 12:33:12 +0400

    123

--- homeworks.js
+++ homeworks.js
@@ -1,6 +1,11 @@
 
 
 let arr = [2,3,576,'45',false,[],{}];
+let arr = [2,3,576,'45',false,[],{}];
+let arr = [2,3,576,'45',false,[],{}];
+let arr = [2,3,576,'45',false,[],{}];
+let arr = [2,3,576,'45',false,[],{}];
+
 
 
 console.log(arr);
